99da478e385b9206f6cc9123d4734c54246e19c6
exoriri <lavrikrom2@gmail.com>
2023-03-19 20:31:21 +0300

    feat: production url

--- client/src/server.js
+++ client/src/server.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const server = axios.create({
-  baseURL: "http://localhost:3042",
+  baseURL: import.meta.env.MODE === 'development' ? 
+    "http://localhost:3042" : 'https://week1-ecdsa-node.vercel.app/',
 });
 
 export default server;
